b876e571da97568578c63fab7e6dd51d51b6c0db
antipers <antipers@gmail.com>
2024-06-02 18:49:59 +0300

    HW5 1st task probably minor fixes

--- src/components/theme-button/component.jsx
+++ src/components/theme-button/component.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { ThemeContext } from "../../contexts/theme-context";
 import { useEffect } from "react";
 
+
 export const SwitchThemeButton = ({ onChange }) => {
   const context = useContext(ThemeContext);
 
@@ -12,9 +13,8 @@ export const SwitchThemeButton = ({ onChange }) => {
       (target) => (target.style.backgroundColor = context)
     );
   }, [context]);
-
   let title;
-  context === true ? (title = "Day mode") : (title = "Night mode");
+  context === "gray" ? (title = "Day mode") : (title = "Night mode");
 
   return <button onClick={onChange}>Switch Buttons theme to {title}</button>;
 };
