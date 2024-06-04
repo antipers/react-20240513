879ceff0d591da86c330bd06055e32c00e6fb96e
antipers <antipers@gmail.com>
2024-06-04 17:24:43 +0300

    Revert "HW5 1st task probably minor fixes"
    This reverts commit b876e571da97568578c63fab7e6dd51d51b6c0db.

--- src/components/theme-button/component.jsx
+++ src/components/theme-button/component.jsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { ThemeContext } from "../../contexts/theme-context";
 import { useEffect } from "react";
 
-
 export const SwitchThemeButton = ({ onChange }) => {
   const context = useContext(ThemeContext);
 
@@ -13,8 +12,9 @@ export const SwitchThemeButton = ({ onChange }) => {
       (target) => (target.style.backgroundColor = context)
     );
   }, [context]);
+
   let title;
-  context === "gray" ? (title = "Day mode") : (title = "Night mode");
+  context === true ? (title = "Day mode") : (title = "Night mode");
 
   return <button onClick={onChange}>Switch Buttons theme to {title}</button>;
 };
