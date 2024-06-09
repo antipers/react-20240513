830be7c49e856f8acdce6d06541bc24d3e21a508
antipers <antipers@gmail.com>
2024-06-09 16:49:54 +0300

    created cart component

--- src/components/dish/container.jsx
+++ src/components/dish/container.jsx
@@ -1,12 +1,38 @@
 import { useSelector } from "react-redux";
 import { Dish } from "./component";
 import { selectDishById } from "../../redux/entities/dish/selectors";
+import { selectDishCount } from "../../redux/ui/cart/selectors";
+import { useDispatch } from "react-redux";
+import { useCallback } from "react";
+import { decrement, increment } from "../../redux/ui/cart";
 
 export const DishContainer = ({ id, ...props }) => {
   const dish = useSelector((state) => selectDishById(state, id));
 
+  const count = useSelector((state) => selectDishCount(state, id));
+  
+
+  const dispatch = useDispatch();
+  const HandleIncrement = useCallback(
+    () => dispatch(increment(id)),
+    [id, dispatch]
+  );
+  const HandleDecrement = useCallback(
+    () => dispatch(decrement(id)),
+    [id, dispatch]
+  );
+  /* const HandleClear = () => dispatch(clear(dish)); */
+
   if (!dish) {
     return null;
   }
-  return <Dish {...props} dish={dish} />;
+  return (
+    <Dish
+      {...props}
+      dish={dish}
+      count={count}
+      HandleDecrement={HandleDecrement}
+      HandleIncrement={HandleIncrement}
+    />
+  );
 };
