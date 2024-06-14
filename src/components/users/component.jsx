d12954f2861b95e7776da98343aa0eb6bf557b2e
antipers <antipers@gmail.com>
2024-06-14 11:51:25 +0300

    fixed crashing during downloading users

--- src/components/users/component.jsx
+++ src/components/users/component.jsx
@@ -1,14 +1,9 @@
 import styles from "./styles.module.css";
 
 export const Users = ({ users }) => {
-/* Без этой проверки все крашится. Пользователи не успевают прийти с сервера */
-  if (!users) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className={styles.root}>
-      <span>{users.name}</span>
+      <span>{users?.name}</span>
     </div>
   );
 };
